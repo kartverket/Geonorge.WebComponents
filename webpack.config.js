74db49bb3da550e622bf030e121df208016b9c6a
Benjamin Dehli <superelg@gmail.com>
2020-09-22 14:55:26 +0200

    Add configuration for webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const ROOT = path.resolve(__dirname, 'src');
+const DESTINATION = path.resolve(__dirname, 'public');
+
+module.exports = {
+   context: ROOT,
+   entry: {
+      'main': './index.ts'
+   },
+   output: {
+      path: `${DESTINATION}/lib`,
+      filename: '[name].js',
+      library: 'geonorge',
+      libraryTarget: 'umd'
+   },
+   resolve: {
+      extensions: ['.ts', '.js'],
+      modules: [
+         ROOT,
+         'node_modules'
+      ]
+   },
+   module: {
+      rules: [
+         {
+            test: /\.html$/i,
+            use: 'raw-loader',
+         },
+         {
+            test: /\.s[ac]ss$/i,
+            use: [
+              'css-loader',
+              'sass-loader',
+            ],
+         },
+         {
+            enforce: 'pre',
+            test: /\.js$/,
+            use: 'source-map-loader'
+         },
+         {
+            test: /\.ts$/,
+            exclude: [/node_modules/],
+            use: 'awesome-typescript-loader'
+         }
+      ]
+   },
+   devtool: 'cheap-module-source-map',
+   devServer: {}
+};
+
