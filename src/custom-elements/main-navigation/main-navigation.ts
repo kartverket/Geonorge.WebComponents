202c0e4e404f9a0109928d0bd80bfe1d53a39917
Benjamin Dehli <superelg@gmail.com>
2020-09-23 17:30:08 +0200

    Remove unused code

--- src/custom-elements/main-navigation/main-navigation.ts
+++ src/custom-elements/main-navigation/main-navigation.ts
@@ -1,17 +1,18 @@
-import { Component, CustomElement, CustomElementOptions, Prop, Dispatch, DispatchEmitter, 
-    Listen, Watch, getElement, getShadowRootElement } from 'super-custom-elements';
+import {
+   Component, CustomElement, CustomElementOptions, Prop, Dispatch, DispatchEmitter,
+   Listen, Watch, getElement, getShadowRootElement
+} from 'super-custom-elements';
 
 import { fetchMenuItems } from 'functions/apiHelpers';
-  
+
   interface MainNavigationOptions extends CustomElementOptions {
      active?: boolean,
-     onClick?: () => void
-  }
+   onClick?: () => void
+}
 
-
-  interface MenuItem extends Object {
-     Name: string,
-     SubMenuItem: Array<MenuItem>,
+interface MenuItem extends Object {
+   Name: string,
+   SubMenuItem: Array<MenuItem>,
      Url: string
   }
   
@@ -26,18 +27,15 @@ import { fetchMenuItems } from 'functions/apiHelpers';
      private searchField: HTMLInputElement;   
      private menuButton: HTMLInputElement;
   
-     @Prop() id: string;
-     @Prop() showMenu: boolean;
-     @Prop() searchString: string;
-     @Prop() public data: Promise<any>;
-     @Prop() language: string;
-     @Prop() menuItems: Array<MenuItem>;
-     @Dispatch('textChanged') onTextChanged: DispatchEmitter;
+   @Prop() id: string;
+   @Prop() showMenu: boolean;
+   @Prop() searchString: string;
+   @Prop() language: string;
+   @Prop() menuItems: Array<MenuItem>;
+   @Dispatch('textChanged') onTextChanged: DispatchEmitter;
   
      constructor() {
         super();
-        console.log("constructor");
-        //https://www.test.geonorge.no/api/menu?omitLinks=1
      }
   
      setup(options?: MainNavigationOptions): void {
@@ -45,23 +43,11 @@ import { fetchMenuItems } from 'functions/apiHelpers';
         if (options.id) {
            this.id = options.id;
         }
-       /* if (options.value) {
-           this.showMenu = options.showMenu;
         }
-        if (options.onTextChanged) {
-           this.addEventListener('textChanged', options.onTextChanged);
-        }
-        if (options.data) {
-           this.data = options.data;
-        }*/
-     }
 
-
-  
      connectedCallback() {
          this.searchField = getShadowRootElement(this, 'input');
         
-         console.log(this.language);
          fetchMenuItems(this.language).then(menuItems => {
             this.menuItems = menuItems;
          });
@@ -74,59 +60,36 @@ import { fetchMenuItems } from 'functions/apiHelpers';
      anchorClicked(event: KeyboardEvent) {
         this.onTextChanged.emit({ detail: event.key });
      }
-  
-     @Watch('value')
-     valueChanged() {
-      if (this.searchField){
-        this.searchField.setAttribute('value', this.searchString)
+
+   @Watch('value')
+   valueChanged() {
+      if (this.searchField) {
+         this.searchField.setAttribute('value', this.searchString)
       }
-     }
+   }
 
      renderMenuItems = (menuItems: Array<MenuItem>) => {
         const menuItemsListElement = menuItems.map((menuItem: MenuItem) => {
          const subItems = menuItem.SubMenuItem && menuItem.SubMenuItem.length ? menuItem.SubMenuItem : null;
          const menuItemElement = `<a href="${menuItem.Url}">${menuItem.Name}</a>`;
          const subItemElements = subItems ? this.renderMenuItems(subItems) : '';
-
          return `<li>${menuItemElement}${subItemElements}</li>`;
-         
-        
-        }).join('');
+      }).join('');
+      return `<ul>${menuItemsListElement}</ul>`;
+   }
 
-        return `<ul>${menuItemsListElement}</ul>`;
+   @Watch('menuItems')
+   menuItemsChanged() {
+      if (this.menuItems && this.menuItems.length) {
+         const template = document.createElement('template');
+         template.innerHTML = this.renderMenuItems(this.menuItems);
 
-     }
-
-     @Watch('menuItems')
-     menuItemsChanged() {
-        if (this.menuItems && this.menuItems.length){
-           console.log("hoy", this.menuItems);
-           const template = document.createElement('template');
-           const li = this.menuItems.map((menuItem) => `<li><a href="${menuItem.Url}">${menuItem.Name}</a></li>`).join('');
-           //template.innerHTML = `<ul>${li}</ul>`;
-           template.innerHTML = this.renderMenuItems(this.menuItems);
-
-           this.menuItems.map(menuItem => {
-              
-
-           })
-     
            const div = getShadowRootElement(this, 'div');
-           div.appendChild(template.content.cloneNode(true));
-   
-        }
-     }
-  
-     @Watch('data')
-     async loadData() {
-        if (this.data === null) {
-           return;
-        }
-  
-        
-     }
-  
-     public static setup(selector: string, options: MainNavigationOptions) {
+         div.appendChild(template.content.cloneNode(true));
+      }
+   }
+
+   public static setup(selector: string, options: MainNavigationOptions) {
         const element = getElement<MainNavigation>(selector);
   
         if (options.onClick) {
