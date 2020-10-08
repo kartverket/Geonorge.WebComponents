7d74800f3d322a8deb7002869c019c56051d682a
Benjamin Dehli <superelg@gmail.com>
2020-10-08 15:47:32 +0200

    Add dummy data for localStorage to demo

---
+++ demo/localStorage.js
@@ -0,0 +1,20 @@
+localStorage.orderItems = JSON.stringify([
+    "041f1e6e-bdbc-4091-b48f-8a5990f3cc5b",
+    "ab61e547-da05-4f4c-8828-9ad87bc20b0b",
+    "a8456aed-441a-40c4-831f-46bcbe4e6ff1"
+]);
+
+localStorage["a8456aed-441a-40c4-831f-46bcbe4e6ff1.metadata"] = JSON.stringify({
+    name: "Arter av nasjonal forvaltningsinteresse",
+    uuid: "a8456aed-441a-40c4-831f-46bcbe4e6ff1"
+});
+
+localStorage["ab61e547-da05-4f4c-8828-9ad87bc20b0b.metadata"] = JSON.stringify({
+    name: "Corine Landcover 2012 (CLC) - Arealdekke",
+    uuid: "ab61e547-da05-4f4c-8828-9ad87bc20b0b"
+});
+
+localStorage["041f1e6e-bdbc-4091-b48f-8a5990f3cc5b.metadata"] = JSON.stringify({
+    name: "Administrative enheter kommuner (gjeldende)",
+    uuid: "041f1e6e-bdbc-4091-b48f-8a5990f3cc5b"
+});
