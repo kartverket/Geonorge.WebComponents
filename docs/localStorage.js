9545ee5c6496e551e36c6b45347b245edda03e35
Benjamin Dehli <superelg@gmail.com>
2020-10-09 13:41:38 +0200

    Build docs

---
+++ docs/localStorage.js
@@ -0,0 +1,20 @@
+localStorage.orderItems = JSON.stringify([
+    "11111111-1111-1111-1111-111111111111",
+    "22222222-2222-2222-2222-222222222222",
+    "33333333-3333-3333-3333-333333333333"
+]);
+
+localStorage["11111111-1111-1111-1111-111111111111.metadata"] = JSON.stringify({
+    name: "Dataset item 1",
+    uuid: "11111111-1111-1111-1111-111111111111"
+});
+
+localStorage["22222222-2222-2222-2222-222222222222.metadata"] = JSON.stringify({
+    name: "Dataset item 2",
+    uuid: "22222222-2222-2222-2222-222222222222"
+});
+
+localStorage["33333333-3333-3333-3333-333333333333.metadata"] = JSON.stringify({
+    name: "Dataset item 3",
+    uuid: "33333333-3333-3333-3333-333333333333"
+});
