6c92c0ac2b39583a66c6c3cbf997d2af17c1cf7c
Benjamin Dehli <superelg@gmail.com>
2020-10-09 12:59:44 +0200

    Add logic and template for removable download items

--- src/custom-elements/main-navigation/download-items/download-items.ts
+++ src/custom-elements/main-navigation/download-items/download-items.ts
@@ -1,124 +1,163 @@
 // Dependencies
 import {
-    Component, CustomElement, CustomElementOptions, Prop, Dispatch, DispatchEmitter,
-    Listen, Watch, getElement, getShadowRootElement, Toggle
- } from 'super-custom-elements';
- 
- 
- // Assets
- import DownloadIcon from 'assets/svg/download-icon.svg';
- 
- // Functions
- import { getDownloadItems, getDownloadItemMetadata } from 'functions/downloadItemHelpers';
- 
- interface DownloadItem extends Object {
-    name: string,
-    organizationName: string,
-    uuid: string
- }
+   Component, CustomElement, CustomElementOptions, Prop, Dispatch, DispatchEmitter,
+   Listen, Watch, getElement, getShadowRootElement, Toggle
+} from 'super-custom-elements';
 
- interface DownloadItemsOptions extends CustomElementOptions {
-    active?: boolean,
-    onClick?: () => void
- }
-  
- @Component({
-    tag: 'download-items',
-    template: import('./download-items.html'),
-    style: import('./download-items.scss')
- })
- 
- export class DownloadItems extends CustomElement {
-    private static readonly elementSelector = 'download-items';
-    private downloadButton: HTMLButtonElement;
-    private downloadIcon: HTMLElement;
-    private downloadIconCounter: HTMLSpanElement;
-    private downloadItemListContainer: HTMLDivElement;
-    private downloadItems:  Array<DownloadItem>;
- 
-    @Prop() id: string;
-    @Toggle() showList: boolean;
- 
-    constructor() {
-       super();
-       this.clickOutsideDownloadItemsContainer = this.clickOutsideDownloadItemsContainer.bind(this);
-    }
- 
-    setup(options?: DownloadItemsOptions): void {
-       this.connect(options.container);
-       if (options.id) {
-          this.id = options.id;
-       }
-    }
- 
-    connectedCallback() {
-       this.downloadButton = getShadowRootElement(this, '#download-toggle-button');
-       this.downloadIcon = getShadowRootElement(this, '#download-toggle-button-icon');
-       this.downloadIconCounter = getShadowRootElement(this, '#download-toggle-button-counter');
-       this.downloadItemListContainer = getShadowRootElement(this, '#download-item-list-container');
-       
-       const downloadItemUuids = getDownloadItems();
-       if (downloadItemUuids && downloadItemUuids.length) {
-          this.downloadItems = downloadItemUuids.map(uuid => {
-             return getDownloadItemMetadata(uuid);
-          })
-       }
-       this.downloadIcon.innerHTML = DownloadIcon;
- 
-       this.downloadIconCounter.innerHTML = this.downloadItems && this.downloadItems.length ? this.downloadItems.length.toString() : '';
- 
-       document.addEventListener('click', this.clickOutsideDownloadItemsContainer);
-    }
- 
-    disconnectedCallback() {
-       document.removeEventListener('click', this.clickOutsideDownloadItemsContainer);
-    }
- 
-    hideListContainer = () => {
-       this.showList = false;
-    }
- 
-    clickOutsideDownloadItemsContainer(event: MouseEvent){
-       const targetElement = event.composedPath()[0] as Element;
-       if (targetElement.closest('#download-item-list-container') || targetElement.closest('#download-toggle-button')) return
-          this.hideListContainer();
-    }
- 
-    renderDownloadItems = (downloadItems: Array<DownloadItem>) => {
-       const downloadItemsListElement = downloadItems.map((downloadItem: DownloadItem) => {
-          const downloadItemElement = `<span>${downloadItem.name}</span>`;
-          return `<li>${downloadItemElement}</li>`;
-       }).join('');
-       return `<ul>${downloadItemsListElement}</ul>`;
-    }
- 
-    @Listen('click', '#download-toggle-button')
-    buttonClicked(event: MouseEvent) {
-       this.showList = !this.showList;
-    }
- 
-    @Watch('showlist')
-    showMenuChanged() {
-       this.showList ? this.downloadItemListContainer.classList.add('open') : this.downloadItemListContainer.classList.remove('open');
-       this.showList ? this.downloadButton.classList.add('open') : this.downloadButton.classList.remove('open');
-    }
- 
-    @Watch('downloaditems')
-    menuItemsChanged() {
-       if (this.downloadItems && this.downloadItems.length) {
-          this.downloadItemListContainer.innerHTML = this.renderDownloadItems(this.downloadItems);
-       }
-    }
- 
-    public static setup(selector: string, options: DownloadItemsOptions) {
-       const element = getElement<DownloadItems>(selector);
- 
-       if (options.onClick) {
-          element.addEventListener('menuButtonClick', options.onClick);
-       }
-       if (options.active) {
-          element.showList = options.active;
-       }
-    }
- }
- 
+
+// Assets
+import DownloadIcon from 'assets/svg/download-icon.svg';
+import TrashIcon from 'assets/svg/trash-icon.svg';
+
+// Functions
+import { getDownloadItems, getDownloadItemMetadata, removeDownloadItem } from 'functions/downloadItemHelpers';
+
+interface DownloadItem extends Object {
+   name: string,
+   organizationName: string,
+   uuid: string
+}
+
+interface DownloadItemsOptions extends CustomElementOptions {
+   active?: boolean,
+   onClick?: () => void
+}
+
+@Component({
+   tag: 'download-items',
+   template: import('./download-items.html'),
+   style: import('./download-items.scss')
+})
+
+export class DownloadItems extends CustomElement {
+   private static readonly elementSelector = 'download-items';
+   private downloadButton: HTMLButtonElement;
+   private downloadIcon: HTMLElement;
+   private downloadIconCounter: HTMLSpanElement;
+   private downloadItemListContainer: HTMLDivElement;
+   private downloadItems: Array<DownloadItem>;
+
+   @Prop() id: string;
+   @Toggle() showList: boolean;
+
+   constructor() {
+      super();
+      this.clickOutsideDownloadItemsContainer = this.clickOutsideDownloadItemsContainer.bind(this);
+   }
+
+   setup(options?: DownloadItemsOptions): void {
+      this.connect(options.container);
+      if (options.id) {
+         this.id = options.id;
+      }
+   }
+
+   connectedCallback() {
+      this.downloadButton = getShadowRootElement(this, '#download-toggle-button');
+      this.downloadIcon = getShadowRootElement(this, '#download-toggle-button-icon');
+      this.downloadIconCounter = getShadowRootElement(this, '#download-toggle-button-counter');
+      this.downloadItemListContainer = getShadowRootElement(this, '#download-item-list-container');
+
+      this.getUpdatedDownloadItems();
+
+      this.downloadIcon.innerHTML = DownloadIcon;
+
+      document.addEventListener('click', this.clickOutsideDownloadItemsContainer);
+   }
+
+   getUpdatedDownloadItems(){
+      const downloadItemUuids = getDownloadItems();
+      this.downloadItems = downloadItemUuids.map(uuid => {
+         return getDownloadItemMetadata(uuid);
+      })
+   }
+
+   disconnectedCallback() {
+      document.removeEventListener('click', this.clickOutsideDownloadItemsContainer);
+   }
+
+   hideListContainer = () => {
+      this.showList = false;
+   }
+
+   clickOutsideDownloadItemsContainer(event: MouseEvent) {
+      const targetElement = event.composedPath()[0] as Element;
+      if (targetElement.closest('#download-item-list-container') || targetElement.closest('#download-toggle-button')) return
+      this.hideListContainer();
+   }
+
+   renderDownloadItemsCounter(){
+      if (this.downloadItems && this.downloadItems.length){
+         this.downloadIconCounter.innerHTML = this.downloadItems.length.toString();
+         this.downloadIconCounter.classList.remove('hidden');
+      } else {
+         this.downloadIconCounter.innerHTML = '';
+         this.downloadIconCounter.classList.add('hidden');
+      }
+   }
+
+   renderDownloadItems = (downloadItems: Array<DownloadItem>) => {
+      const downloadItemsListElement = downloadItems.map((downloadItem: DownloadItem) => {
+         const downloadItemElement = document.createElement('span');
+         downloadItemElement.innerText = downloadItem.name;
+
+         const removeDownloadItemElement = document.createElement('button');
+         removeDownloadItemElement.classList.add('list-icon');
+         removeDownloadItemElement.innerHTML = TrashIcon;
+         removeDownloadItemElement.dataset['downloadItem'] = JSON.stringify(downloadItem);
+         downloadItemElement.appendChild(removeDownloadItemElement);
+
+         return `<li>${downloadItemElement.innerHTML}</li>`;
+      }).join('');
+      this.downloadItemListContainer.innerHTML = `<ul>${downloadItemsListElement}</ul>`;
+   }
+
+   @Listen('click', '#download-toggle-button')
+   buttonClicked(event: MouseEvent) {
+      this.showList = !this.showList;
+   }
+
+   @Listen('click', '#download-item-list-container')
+   removeDownloadItemClicked(event: MouseEvent) {
+      const targetElement = event.composedPath()[0] as Element;
+      targetElement.closest('#menu-container');
+      const removeDownloadItemButtonElement = targetElement.closest('button');
+
+      if (removeDownloadItemButtonElement) {
+         const downloadItem = removeDownloadItemButtonElement.dataset && removeDownloadItemButtonElement.dataset.downloadItem ? JSON.parse(removeDownloadItemButtonElement.dataset.downloadItem) : null;
+         if (downloadItem) {
+            event.stopPropagation();
+            removeDownloadItem(downloadItem);
+            this.getUpdatedDownloadItems();
+            this.renderDownloadItems(this.downloadItems);
+            this.renderDownloadItemsCounter();
+         }
+      }
+   }
+
+   @Watch('showlist')
+   showMenuChanged() {
+      this.showList ? this.downloadItemListContainer.classList.add('open') : this.downloadItemListContainer.classList.remove('open');
+      this.showList ? this.downloadButton.classList.add('open') : this.downloadButton.classList.remove('open');
+   }
+
+   @Watch('downloadItems')
+   downloadItemsChanged() {
+      console.log("downloadItemsChanged", this.downloadItems);
+      if (this.downloadItems && this.downloadItems.length) {
+         this.renderDownloadItems(this.downloadItems);
+         this.renderDownloadItemsCounter();
+      }
+   }
+
+   public static setup(selector: string, options: DownloadItemsOptions) {
+      const element = getElement<DownloadItems>(selector);
+
+      if (options.onClick) {
+         element.addEventListener('menuButtonClick', options.onClick);
+      }
+      if (options.active) {
+         element.showList = options.active;
+      }
+   }
+}
--- src/functions/downloadItemHelpers.ts
+++ src/functions/downloadItemHelpers.ts
@@ -9,3 +9,23 @@ export const getDownloadItemMetadata = (itemUuid: string) => {
         ? JSON.parse(localStorage[itemUuid + '.metadata'])
         : null;
 }
+
+export const removeDownloadItem = (itemToRemove) => {
+    let selectedItems = localStorage.orderItems && Array.isArray(JSON.parse(localStorage.orderItems))
+      ? JSON.parse(localStorage.orderItems)
+      : [];
+    console.log("selectedItems", selectedItems);
+    localStorage.orderItems = JSON.stringify(selectedItems.filter(itemToKeep => itemToKeep !== itemToRemove.uuid));
+  
+    localStorage.removeItem(itemToRemove.uuid + ".metadata")
+
+
+    /*const tagData = {
+      name: itemToRemove.name,
+      uuid: itemToRemove.uuid,
+      accessIsOpendata: itemToRemove.accessIsOpendata,
+      accessIsRestricted: itemToRemove.accessIsRestricted,
+      organizationName: itemToRemove.organizationName,
+      theme: itemToRemove.theme
+    };*/
+  }
