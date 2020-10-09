db65abb5be93f80cf93145a0ca0ddc134f09667a
Benjamin Dehli <superelg@gmail.com>
2020-10-09 13:09:02 +0200

    Adjust dummy data for localStorage

--- demo/localStorage.js
+++ demo/localStorage.js
@@ -1,20 +1,20 @@
 localStorage.orderItems = JSON.stringify([
-    "041f1e6e-bdbc-4091-b48f-8a5990f3cc5b",
-    "ab61e547-da05-4f4c-8828-9ad87bc20b0b",
-    "a8456aed-441a-40c4-831f-46bcbe4e6ff1"
+    "11111111-1111-1111-1111-111111111111",
+    "22222222-2222-2222-2222-222222222222",
+    "33333333-3333-3333-3333-333333333333"
 ]);
 
-localStorage["a8456aed-441a-40c4-831f-46bcbe4e6ff1.metadata"] = JSON.stringify({
-    name: "Arter av nasjonal forvaltningsinteresse",
-    uuid: "a8456aed-441a-40c4-831f-46bcbe4e6ff1"
+localStorage["11111111-1111-1111-1111-111111111111.metadata"] = JSON.stringify({
+    name: "Dataset item 1",
+    uuid: "11111111-1111-1111-1111-111111111111"
 });
 
-localStorage["ab61e547-da05-4f4c-8828-9ad87bc20b0b.metadata"] = JSON.stringify({
-    name: "Corine Landcover 2012 (CLC) - Arealdekke",
-    uuid: "ab61e547-da05-4f4c-8828-9ad87bc20b0b"
+localStorage["22222222-2222-2222-2222-222222222222.metadata"] = JSON.stringify({
+    name: "Dataset item 2",
+    uuid: "22222222-2222-2222-2222-222222222222"
 });
 
-localStorage["041f1e6e-bdbc-4091-b48f-8a5990f3cc5b.metadata"] = JSON.stringify({
-    name: "Administrative enheter kommuner (gjeldende)",
-    uuid: "041f1e6e-bdbc-4091-b48f-8a5990f3cc5b"
+localStorage["33333333-3333-3333-3333-333333333333.metadata"] = JSON.stringify({
+    name: "Dataset item 3",
+    uuid: "33333333-3333-3333-3333-333333333333"
 });
