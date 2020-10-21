ce253bc9afe0af66fffe6393770ed5779c481b11
Benjamin Dehli <superelg@gmail.com>
2020-10-21 15:40:25 +0200

    Add helper functions for cookies

---
+++ src/functions/cookieHelpers.ts
@@ -0,0 +1,30 @@
+export const getCookie = (cname: string) => {
+    const name = `${cname}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+export const setCookie = (cname: string, cvalue: string, exdays: number) => {
+    let expireDate = new Date();
+    expireDate.setTime(expireDate.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    const expires = `expires=${expireDate.toUTCString()}`;
+    document.cookie = `${cname}=${cvalue};${expires};path=/;`;
+}
+
+export const getLanguage = () => {
+    return getCookie('_culture');
+}
+
+export const setLanguage = (language: string) => {
+    setCookie('_culture', language, 7);
+}
