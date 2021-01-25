fae6b09de54f71ef8f72166aa941dc89a79bcef3
Benjamin Dehli <superelg@gmail.com>
2021-01-25 10:10:31 +0100

    Add polyfill script for Edge browsers

--- src/init.ts
+++ src/init.ts
@@ -8,9 +8,17 @@ WebFont.load({
 });
 
 
+const head: HTMLHeadElement = document.head || document.getElementsByTagName('head')[0];
+
 // Inject stylesheet
 const css: string = initStyle.toString();
-let head: HTMLHeadElement = document.head || document.getElementsByTagName('head')[0];
 const style: HTMLStyleElement = document.createElement('style');
 head.appendChild(style);
 style.appendChild(document.createTextNode(css));
+
+// Polyfill for Edge legacy browsers
+if (window.navigator.userAgent.indexOf("Edge/") > -1) {
+    const polyfillScript = document.createElement('script');
+    polyfillScript.src = "https://cdn.jsdelivr.net/npm/@webcomponents/webcomponentsjs@2.5.0/webcomponents-bundle.min.js";
+    head.appendChild(polyfillScript);
+};
