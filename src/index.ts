0cd6fcd0ce0e0f4dae863be03f1819d1a00f678e
Benjamin Dehli <superelg@gmail.com>
2022-08-29 11:12:30 +0200

    Add component for breadcrumb list

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 export * from './init';
 
+export * from './custom-elements/breadcrumb-list/breadcrumb-list';
+
+export * from './custom-elements/data-table/data-table';
+
 export * from './custom-elements/geonorge-footer/geonorge-footer';
 
 export * from './custom-elements/main-navigation/main-navigation';
@@ -12,7 +16,5 @@ export * from './custom-elements/main-navigation/main-menu/main-menu';
 export * from './custom-elements/navigation-tabs/navigation-tabs';
 export * from './custom-elements/navigation-tabs/navigation-tab-heading/navigation-tab-heading';
 export * from './custom-elements/navigation-tabs/navigation-tab-content/navigation-tab-content';
-
-export * from './custom-elements/data-table/data-table';
 
 export * from './custom-elements/standard-button/standard-button';
