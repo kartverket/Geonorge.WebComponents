d6f3204f9f6423292a9e6836b9fef6c77f7fd8b7
Benjamin Dehli <superelg@gmail.com>
2022-09-05 10:39:01 +0200

    Add missing export for body text component

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
-export * from './init';
-
-export * from './custom-elements/breadcrumb-list/breadcrumb-list';
-
-export * from './custom-elements/data-table/data-table';
-
-export * from './custom-elements/geonorge-footer/geonorge-footer';
-
-export * from './custom-elements/heading-text/heading-text';
-
-export * from './custom-elements/main-navigation/main-navigation';
-export * from './custom-elements/main-navigation/main-search-field/main-search-field';
-export * from './custom-elements/main-navigation/search-type-selector/search-type-selector';
-export * from './custom-elements/main-navigation/download-items/download-items';
-export * from './custom-elements/main-navigation/map-items/map-items';
-export * from './custom-elements/main-navigation/main-menu/main-menu';
-
-export * from './custom-elements/navigation-tabs/navigation-tabs';
-export * from './custom-elements/navigation-tabs/navigation-tab-heading/navigation-tab-heading';
-export * from './custom-elements/navigation-tabs/navigation-tab-content/navigation-tab-content';
-
-export * from './custom-elements/standard-button/standard-button';
+export * from "./init";
+export * from "./custom-elements/body-text/body-text";
+export * from "./custom-elements/breadcrumb-list/breadcrumb-list";
+export * from "./custom-elements/data-table/data-table";
+export * from "./custom-elements/geonorge-footer/geonorge-footer";
+export * from "./custom-elements/heading-text/heading-text";
+export * from "./custom-elements/main-navigation/main-navigation";
+export * from "./custom-elements/main-navigation/main-search-field/main-search-field";
+export * from "./custom-elements/main-navigation/search-type-selector/search-type-selector";
+export * from "./custom-elements/main-navigation/download-items/download-items";
+export * from "./custom-elements/main-navigation/map-items/map-items";
+export * from "./custom-elements/main-navigation/main-menu/main-menu";
+export * from "./custom-elements/navigation-tabs/navigation-tabs";
+export * from "./custom-elements/navigation-tabs/navigation-tab-heading/navigation-tab-heading";
+export * from "./custom-elements/navigation-tabs/navigation-tab-content/navigation-tab-content";
+export * from "./custom-elements/standard-button/standard-button";
