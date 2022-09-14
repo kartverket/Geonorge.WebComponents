7f03c4d9338fbc8d0d75fad44893d61fc3e59b0a
Benjamin Dehli <superelg@gmail.com>
2022-09-14 11:05:14 +0200

    Add helper function for appending global styles

--- src/functions/guiHelpers.ts
+++ src/functions/guiHelpers.ts
@@ -1,3 +1,12 @@
-export const getFocusableElementsInsideElement = (element) => {
+export const getFocusableElementsInsideElement = (element: HTMLElement) => {
     return element.querySelectorAll('button, [href], input, [tabindex="0"]');
+};
+
+export const addGlobalStylesheet = (styleElementId: string, styles: string) => {
+    const style = document.createElement("style");
+    style.setAttribute("id", styleElementId);
+    style.textContent = styles;
+    if (!document.getElementById(styleElementId)) {
+        document.head.appendChild(style);
+    }
 };
