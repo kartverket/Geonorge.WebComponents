a6a4cbcf3f0f9c7f3b4da52d55730ea9c93b25ad
Ole <ole@arkitektum.no>
2022-12-07 13:28:32 +0100

    Added styling to content-container

--- src/stories/content-container/content-container.ts
+++ src/stories/content-container/content-container.ts
@@ -1,17 +1,19 @@
 // Dependencies
+import { addGlobalStylesheet } from "../../functions/guiHelpers";
 import { Component, CustomElement, CustomElementOptions } from "super-custom-elements";
+import style from "./content-container.scss";
 
 interface ContentContainerOptions extends CustomElementOptions {}
 
 @Component({
     tag: "content-container",
-    template: import("./content-container.html"),
-    style: import("./content-container.scss")
+    template: import("./content-container.html")   
 })
 export class ContentContainer extends CustomElement {
 
     constructor() {
         super();
+        addGlobalStylesheet("content-container-styles", style);
     }
 
     setup(options?: ContentContainerOptions): void {}
