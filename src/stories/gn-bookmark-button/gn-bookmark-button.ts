797bc0c36e39c102ec03e0835153e3647afcbb05
Benjamin Dehli <superelg@gmail.com>
2025-04-14 15:22:57 +0200

    WIP

--- src/stories/gn-bookmark-button/gn-bookmark-button.ts
+++ src/stories/gn-bookmark-button/gn-bookmark-button.ts
@@ -3,6 +3,7 @@ import { Component, CustomElement, CustomElementOptions, getShadowRootElement, P
 
 // Helpers
 import { addGlobalStylesheet, getDocumentHeading } from "../../functions/guiHelpers";
+import { deleteShortcutItem, fetchShortcutItems, postShortcutItem } from "../../functions/apiHelpers";
 
 // Assets
 import StarIcon from "../../assets/svg/star.svg";
@@ -22,6 +23,8 @@ export class GnBookmarkButton extends CustomElement {
     @Prop() id: string;
     @Prop() language: string;
     @Prop() environment: string;
+    @Prop() userinfo: string;
+    @Prop() token: string;
 
     constructor() {
         super();
@@ -32,9 +35,59 @@ export class GnBookmarkButton extends CustomElement {
         this.bookmarkButton = getShadowRootElement(this, "#gn-bookmark-button");
         if (this.bookmarkButton) {
             this.appendBookmarkButtonAfterFirstHeading();
+            fetchShortcutItems(this.environment, "this.token").then((shortcutItems) => {
+                const isAlreadyBookmarked = shortcutItems.some((item) => item.url === window.location.href);
+                this.bookmarkButton.innerHTML = isAlreadyBookmarked ? StarIcon : StarIcon;
+                this.bookmarkButton.setAttribute(
+                    "aria-label",
+                    this.language === "en" ? "Remove shortcut to this page" : "Fjern snarvei for denne siden"
+                );
+                this.bookmarkButton.addEventListener("click", () => {
+                    if (isAlreadyBookmarked) {
+                        this.removeBookmark();
+                    }
+                    this.addBookmark();
+                });
+            });
         } else {
             console.error("Bookmark button not found in shadow DOM");
         }
+    }
+
+    disconnectedCallback() {
+        this.bookmarkButton.removeEventListener("click", this.addBookmark);
+    }
+    addBookmark() {
+        const bookmarkData = {
+            name: document.title,
+            url: window.location.href
+        };
+        postShortcutItem(this.environment, this.token, bookmarkData)
+            .then((data) => {
+                console.log("Shortcut added successfully:", data);
+                this.bookmarkButton.innerHTML = StarIcon;
+                this.bookmarkButton.setAttribute(
+                    "aria-label",
+                    this.language === "en" ? "Remove shortcut to this page" : "Fjern snarvei for denne siden"
+                );
+            })
+            .catch((error) => {
+                console.error("Error adding bookmark:", error);
+            });
+    }
+    removeBookmark() {
+        deleteShortcutItem(this.environment, this.token, { url: window.location.href })
+            .then((data) => {
+                console.log("Shortcut removed successfully:", data);
+                this.bookmarkButton.innerHTML = StarIcon;
+                this.bookmarkButton.setAttribute(
+                    "aria-label",
+                    this.language === "en" ? "Add shortcut to this page" : "Lagre snarvei for denne siden"
+                );
+            })
+            .catch((error) => {
+                console.error("Error removing bookmark:", error);
+            });
     }
 
     appendBookmarkButtonAfterFirstHeading() {
