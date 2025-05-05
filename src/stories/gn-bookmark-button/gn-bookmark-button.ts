2ef5957430493f7680139823163243932f7e064d
Benjamin Dehli <superelg@gmail.com>
2025-05-05 15:40:25 +0200

    WIP

--- src/stories/gn-bookmark-button/gn-bookmark-button.ts
+++ src/stories/gn-bookmark-button/gn-bookmark-button.ts
@@ -1,8 +1,18 @@
 // Dependencies
-import { Component, CustomElement, CustomElementOptions, getShadowRootElement, Prop } from "super-custom-elements";
+import {
+    Component,
+    CustomElement,
+    CustomElementOptions,
+    Dispatch,
+    DispatchEmitter,
+    getElement,
+    getShadowRootElement,
+    Prop
+} from "super-custom-elements";
 
 // Helpers
 import { addGlobalStylesheet, getDocumentHeading } from "../../functions/guiHelpers";
+import { deleteShortcutItem, fetchShortcutItems, postShortcutItem } from "../../functions/apiHelpers";
 
 // Assets
 import StarIcon from "../../assets/svg/star.svg";
@@ -10,7 +20,9 @@ import StarIcon from "../../assets/svg/star.svg";
 // Stylesheets
 import style from "./gn-bookmark-button.scss";
 
-interface GnBookmarkButtonOptions extends CustomElementOptions {}
+interface GnBookmarkButtonOptions extends CustomElementOptions {
+    getAuthToken?: () => void;
+}
 
 @Component({
     tag: "gn-bookmark-button",
@@ -18,23 +30,85 @@ interface GnBookmarkButtonOptions extends CustomElementOptions {}
 })
 export class GnBookmarkButton extends CustomElement {
     private bookmarkButton: HTMLButtonElement;
-
     @Prop() id: string;
     @Prop() language: string;
     @Prop() environment: string;
+    @Prop() userinfo: string;
+    @Prop() token: string;
+    @Dispatch("getAuthToken") getAuthToken: DispatchEmitter;
 
     constructor() {
         super();
         addGlobalStylesheet("gn-bookmark-button-styles", style);
     }
 
+    setup(options?: GnBookmarkButtonOptions): void {
+        this.connect(options.container);
+        if (options.id) {
+            this.id = options.id;
+        }
+    }
+
     connectedCallback() {
         this.bookmarkButton = getShadowRootElement(this, "#gn-bookmark-button");
+      
         if (this.bookmarkButton) {
             this.appendBookmarkButtonAfterFirstHeading();
+            // fetchShortcutItems(this.environment, "this.token").then((shortcutItems) => {
+           /* fetchShortcutItems("dev", "this.token").then((shortcutItems) => {
+                // Temporary fix for testing
+                const isAlreadyBookmarked = shortcutItems.some((item) => item.url === window.location.href);
+                this.bookmarkButton.innerHTML = isAlreadyBookmarked ? StarIcon : StarIcon;
+                this.bookmarkButton.setAttribute(
+                    "aria-label",
+                    this.language === "en" ? "Remove shortcut to this page" : "Fjern snarvei for denne siden"
+                );
+                this.bookmarkButton.addEventListener("click", () => {
+                    if (isAlreadyBookmarked) {
+                        this.removeBookmark();
+                    }
+                    this.addBookmark();
+                });
+            });*/
         } else {
             console.error("Bookmark button not found in shadow DOM");
         }
+    }
+
+    disconnectedCallback() {
+       // this.bookmarkButton.removeEventListener("click", this.addBookmark);
+    }
+    addBookmark() {
+        const bookmarkData = {
+            name: document.title,
+            url: window.location.href
+        };
+        postShortcutItem(this.environment, this.token, bookmarkData)
+            .then((data) => {
+                console.log("Shortcut added successfully:", data);
+                this.bookmarkButton.innerHTML = StarIcon;
+                this.bookmarkButton.setAttribute(
+                    "aria-label",
+                    this.language === "en" ? "Remove shortcut to this page" : "Fjern snarvei for denne siden"
+                );
+            })
+            .catch((error) => {
+                console.error("Error adding bookmark:", error);
+            });
+    }
+    removeBookmark() {
+        deleteShortcutItem(this.environment, this.token, { url: window.location.href })
+            .then((data) => {
+                console.log("Shortcut removed successfully:", data);
+                this.bookmarkButton.innerHTML = StarIcon;
+                this.bookmarkButton.setAttribute(
+                    "aria-label",
+                    this.language === "en" ? "Add shortcut to this page" : "Lagre snarvei for denne siden"
+                );
+            })
+            .catch((error) => {
+                console.error("Error removing bookmark:", error);
+            });
     }
 
     appendBookmarkButtonAfterFirstHeading() {
@@ -47,6 +121,16 @@ export class GnBookmarkButton extends CustomElement {
         }
         if (firstHeading && this.bookmarkButton) {
             this.bookmarkButton.innerHTML = StarIcon;
+            this.bookmarkButton.onclick = () => {
+                console.log("Bookmark button clicked");
+                const token = this.getAuthToken.emit();
+                console.log("Token received on click:", token);
+            };
+         /*   this.bookmarkButton.addEventListener("click", () => {
+                console.log("Bookmark button clicked");
+                const token = this.getAuthToken.emit();
+                console.log("Token received on click:", token);
+            });*/
             this.bookmarkButton.setAttribute(
                 "aria-label",
                 this.language === "en" ? "Add shortcut to this page" : "Lagre snarvei for denne siden"
@@ -57,10 +141,13 @@ export class GnBookmarkButton extends CustomElement {
         }
     }
 
-    setup(options?: GnBookmarkButtonOptions): void {
-        this.connect(options.container);
-        if (options.id) {
-            this.id = options.id;
+    public static setup(selector: string, options: GnBookmarkButtonOptions) {
+        const element = getElement<GnBookmarkButton>(selector);
+        if (options.getAuthToken) {
+            setTimeout(() => {
+                const authToken = options.getAuthToken();
+                console.log("Auth token received:", authToken);
+            });
         }
     }
 }
