f01d2a303db7b3d075af8d5096285bb994d36ca8
Benjamin Dehli <superelg@gmail.com>
2025-05-16 13:32:00 +0200

    Improves button color and hover states
    Refactors button styling to use slotted elements and CSS classes for color variations.
    This approach provides better encapsulation and allows for dynamic color updates.
    
    Adds hover states using JavaScript to manage the 'is-hovered' class,
    and manages the `is-disabled` class based on the disabled state of the slotted element.
    
    Renames the stylesheet to `template.scss` for clarity.

--- src/stories/gn-button/gn-button.ts
+++ src/stories/gn-button/gn-button.ts
@@ -1,30 +1,80 @@
-// Dependencies
-import { Component, CustomElement, CustomElementOptions, Prop } from "super-custom-elements";
-
-// Helpers
-import { addGlobalStylesheet } from "../../functions/guiHelpers";
-
-// Stylesheets
-import style from "./gn-button.scss";
+import { Component, CustomElement, CustomElementOptions, Prop, Watch } from "super-custom-elements";
 
 interface GnButtonOptions extends CustomElementOptions {}
 
 @Component({
     tag: "gn-button",
-    template: import("./gn-button.html")
+    template: import("./gn-button.html"),
+    style: import("./template.scss")
 })
 export class GnButton extends CustomElement {
     @Prop() color: string;
 
+    private slotEl: HTMLSlotElement;
+
     constructor() {
         super();
-        addGlobalStylesheet("gn-button-styles", style);
     }
 
     setup(options?: GnButtonOptions): void {
-        this.connect(options.container);
-        if (options.id) {
+        this.connect(options?.container);
+        if (options?.id) {
             this.id = options.id;
         }
+
+        // Wait for slot to be rendered and then apply color
+        requestAnimationFrame(() => this.applyColorClass());
     }
+
+    @Watch("color")
+    onColorChange() {
+        this.applyColorClass();
+    }
+
+    private applyColorClass() {
+        if (!this.slotEl) {
+            this.slotEl = this.shadowRoot?.querySelector("slot");
+            if (!this.slotEl) return;
+        }
+
+        const colorClassPrefix = "color-";
+        const assignedElements = this.slotEl.assignedElements({ flatten: true });
+
+        assignedElements.forEach((el) => {
+            // Clean up any existing listeners first
+            el.removeEventListener("mouseenter", this.handleMouseEnter);
+            el.removeEventListener("mouseleave", this.handleMouseLeave);
+
+            // Remove previous color-* classes
+            el.classList.forEach((cls) => {
+                if (cls.startsWith(colorClassPrefix)) {
+                    el.classList.remove(cls);
+                }
+            });
+
+            // Add color class
+            if (this.color) {
+                el.classList.add(`${colorClassPrefix}${this.color}`);
+            }
+
+            // Add :hover support via JS
+            el.addEventListener("mouseenter", this.handleMouseEnter);
+            el.addEventListener("mouseleave", this.handleMouseLeave);
+
+            // Add disabled class if applicable
+            if ((el as HTMLButtonElement).disabled) {
+                el.classList.add("is-disabled");
+            } else {
+                el.classList.remove("is-disabled");
+            }
+        });
+    }
+
+    private handleMouseEnter = (e: Event) => {
+        (e.currentTarget as HTMLElement).classList.add("is-hovered");
+    };
+
+    private handleMouseLeave = (e: Event) => {
+        (e.currentTarget as HTMLElement).classList.remove("is-hovered");
+    };
 }
